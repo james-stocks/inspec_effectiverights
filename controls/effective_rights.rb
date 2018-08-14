f8eb90fcdbe747db1a68998de8d06ac23cb0f6a4
James Stocks <jstocks@chef.io>
2018-08-14 15:06:21 +0100

    Change sid resource to security_identifier resource
    Require user of resource to provide either :user or :group
    
    Signed-off-by: James Stocks <jstocks@chef.io>

--- controls/effective_rights.rb
+++ controls/effective_rights.rb
@@ -1,5 +1,5 @@
 control 'demonstrate effective rights tests' do
-  administrators = sid('Administrators').to_s
+  administrators = security_identifier(group: 'Administrators').sid
   sys_exes = command('ls C:\\windows\\system32\\EventVwr.exe | % FullName').stdout.tr("\r\n", "\n").split("\n").reject { |blank| blank == "" }
   sys_exes.each do |sys_exe|
     describe file_permissions(sys_exe) do
@@ -7,7 +7,8 @@ control 'demonstrate effective rights tests' do
       its(administrators) { should_not include 'Write' }
       its('Guest') { should eq [] }
       # Guests should be forbidden read and execute
-      its('ReadAndExecute') { should include sid('Administrators').to_s }
+      its('ReadAndExecute') { should include security_identifier(group: 'Administrators').sid }
+      its('ReadAndExecute') { should_not include security_identifier(user: 'Administrator').sid }
       its('ReadAndExecute') { should_not include 'S-1-5-32-546' } 
     end
   end
--- libraries/sid.rb
+++ libraries/sid.rb
@@ -1,25 +1,31 @@
-class Sid < Inspec.resource(1)
+class SecurityIdentifier < Inspec.resource(1)
   # Resource that returns a Security ID for a given entity name in Windows
   # If there is no SID for this entity, echo back the entity name.
-  name 'sid'
+  name 'security_identifier'
 
-  def initialize(name)
-    @name = name
+  def initialize(opts = {})
+    supported_opt_keys = [:user, :group]
+    raise "Invalid security_identifier param '#{opts}'. Please pass a hash with these supported keys: #{supported_opt_keys}" unless opts.respond_to?(:keys)
+    raise "Unsupported security_identifier options '#{opts.keys - supported_opt_keys}'. Supported keys: #[supported_opt_keys]" unless (opts.keys - supported_opt_keys).empty? 
+    @user = opts[:user]
+    @group = opts[:group]
+    raise 'Specify either :user or :group for security_identifier' unless @user || @group 
+    raise 'Specifying both :user and :group for security_identifier is not supported' if @user && @group
     @sids = nil
-    fetch_sids
-    @sid = @sids[@name] || @name
   end
 
-  def to_s
+  def sid
     fetch_sids unless @sids
-    @sid = @sids[@name].to_s || @name
-    @sid
+    entity = @user || @group
+    @sids[entity].to_s || entity
   end
 
   def fetch_sids
     @sids = {}
     # This could be more specific and only query for @name - but with Inspec command caching, it may save time to have all the results cached?
-    sid_data = inspec.command('wmic group get Name","SID /format:csv').stdout.strip.split("\r\n\r\n")[1..-1].map { |entry| entry.split(',') }
+    wmi_query = 'wmic group get Name","SID /format:csv'
+    wmi_query = 'wmic useraccount get Name","SID /format:csv' if @user
+    sid_data = inspec.command(wmi_query).stdout.strip.split("\r\n\r\n")[1..-1].map { |entry| entry.split(',') }
     sid_data.each { |sid| @sids[sid[1]] = sid[2] }
   end
 end
