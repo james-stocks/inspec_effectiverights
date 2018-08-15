8a29338cb81392cb6e05b5f060f431222dd84e6a
James Stocks <jstocks@chef.io>
2018-08-15 11:06:33 +0100

    Simplify example control
    Don't bother with a loop over multiple files
    
    Signed-off-by: James Stocks <jstocks@chef.io>

--- controls/effective_rights.rb
+++ controls/effective_rights.rb
@@ -1,15 +1,13 @@
 control 'demonstrate effective rights tests' do
   administrators = security_identifier(group: 'Administrators').sid
-  sys_exes = command('ls C:\\windows\\system32\\EventVwr.exe | % FullName').stdout.tr("\r\n", "\n").split("\n").reject { |blank| blank == "" }
-  sys_exes.each do |sys_exe|
-    describe file_permissions(sys_exe) do
-      its(administrators) { should include 'ReadAndExecute' }
-      its(administrators) { should_not include 'Write' }
-      its('Guest') { should eq [] }
-      # Guests should be forbidden read and execute
-      its('ReadAndExecute') { should include security_identifier(group: 'Administrators').sid }
-      its('ReadAndExecute') { should_not include security_identifier(user: 'Administrator').sid }
-      its('ReadAndExecute') { should_not include 'S-1-5-32-546' } 
-    end
+
+  describe file_permissions('C:\\windows\\system32\\EventVwr.exe') do
+    its(administrators) { should include 'ReadAndExecute' }
+    its(administrators) { should_not include 'Write' }
+    its('Guest') { should eq [] }
+    # Guests should be forbidden read and execute
+    its('ReadAndExecute') { should include security_identifier(group: 'Administrators').sid }
+    its('ReadAndExecute') { should_not include security_identifier(user: 'Administrator').sid }
+    its('ReadAndExecute') { should_not include 'S-1-5-32-546' } 
   end
 end
