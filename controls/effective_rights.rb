488641ed176ff78cb74af608d47347c372a3d542
James Stocks <jstocks@chef.io>
2018-08-28 11:26:13 +0100

    Support :unspecified SID type
    Signed-off-by: James Stocks <jstocks@chef.io>

--- controls/effective_rights.rb
+++ controls/effective_rights.rb
@@ -8,6 +8,8 @@ control 'demonstrate effective rights tests' do
     # Guests should be forbidden read and execute
     its('ReadAndExecute') { should include security_identifier(group: 'Administrators').sid }
     its('ReadAndExecute') { should_not include security_identifier(user: 'Administrator').sid }
+    its('ReadAndExecute') { should_not include security_identifier(user: 'Doesnt Exist').sid }
+    its('ReadAndExecute') { should include security_identifier(unspecified: 'Administrators').sid }
     its('ReadAndExecute') { should_not include 'S-1-5-32-546' } 
   end
 end
--- libraries/SecurityIdentifier.rb
+++ libraries/SecurityIdentifier.rb
@@ -1,7 +1,7 @@
 class SecurityIdentifier < Inspec.resource(1)
   name 'security_identifier'
   supports platform: 'windows'
-  desc 'Resource that returns a Security Identifier for a given entity name in Windows. Because different entities can have the same name (e.g. a user and group can both be called \'devops\') the resource requires the type of the entity (:user, :group) to be stated to avoid an ambiguous test'
+  desc 'Resource that returns a Security Identifier for a given entity name in Windows. Because different entities can have the same name (e.g. a user and group can both be called \'devops\') the resource requires the type of the entity (:user, :group) to be stated to avoid an ambiguous test. If the entity type is unknown (e.g. when working with SCAP content that names an entity but does not declare its type) you may give the type as :unspecified to explicitly state that you need the resource to try to determine a SID when the entity type is not known.'
   example "
     describe security_policy do
       its(\"SeRemoteInteractiveLogonRight\") { should_not include security_identifier(group: 'Guests') }
@@ -9,27 +9,51 @@ class SecurityIdentifier < Inspec.resource(1)
   "
 
   def initialize(opts = {})
-    supported_opt_keys = [:user, :group]
+    supported_opt_keys = [:user, :group, :unspecified]
     raise "Invalid security_identifier param '#{opts}'. Please pass a hash with these supported keys: #{supported_opt_keys}" unless opts.respond_to?(:keys)
     raise "Unsupported security_identifier options '#{opts.keys - supported_opt_keys}'. Supported keys: #[supported_opt_keys]" unless (opts.keys - supported_opt_keys).empty? 
-    @user = opts[:user]
-    @group = opts[:group]
-    raise 'Specify either :user or :group for security_identifier' unless @user || @group 
-    raise 'Specifying both :user and :group for security_identifier is not supported' if @user && @group
+    raise 'Specifying more than one of :user :group or :unspecified for security_identifier is not supported' unless opts.keys unless (opts.keys & supported_opt_keys).length == 1
+    if opts[:user]
+      @type = :user
+      @name = opts[:user]
+    end
+    if opts[:group]
+      @type = :group
+      @name = opts[:group]
+    end
+    if opts[:unspecified]
+      @type = :unspecified
+      @name = opts[:unspecified]
+    end
+    raise 'Specify one of :user :group or :unspecified for security_identifier' unless @name
     @sids = nil
   end
 
   def sid
     fetch_sids unless @sids
-    entity = @user || @group
-    @sids[entity].to_s || entity
+    @sids[@name] || @name
   end
 
   def fetch_sids
     @sids = {}
-    wmi_query = "wmic group where 'Name=\"#{@group}\"' get Name\",\"SID /format:csv"
-    wmi_query = "wmic useraccount where 'Name=\"#{@user}\"' get Name\",\"SID /format:csv" if @user
-    sid_data = inspec.command(wmi_query).stdout.strip.split("\r\n\r\n")[1..-1].map { |entry| entry.split(',') }
+    case @type
+    when :group
+      sid_data = wmi_results_array("wmic group where 'Name=\"#{@name}\"' get Name\",\"SID /format:csv")
+    when :user
+      sid_data = wmi_results_array("wmic useraccount where 'Name=\"#{@name}\"' get Name\",\"SID /format:csv")
+    when :unspecified
+      # try group first, then user
+      sid_data = wmi_results_array("wmic group where 'Name=\"#{@name}\"' get Name\",\"SID /format:csv" )
+      if sid_data.empty?
+        sid_data = wmi_results_array("wmic useraccount where 'Name=\"#{@name}\"' get Name\",\"SID /format:csv")
+      end
+    else
+      raise "Unhandled entity type '#{@type.to_s}'"
+    end
     sid_data.each { |sid| @sids[sid[1]] = sid[2] }
+  end
+
+  def wmi_results_array(query)
+    inspec.command(query).stdout.strip.split("\r\n\r\n")[1..-1].map { |entry| entry.split(',') }
   end
 end
